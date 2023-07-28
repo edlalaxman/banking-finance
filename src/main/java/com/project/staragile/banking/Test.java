513d7099a7ca7fe79cbf3411247ee83734cdee71
laxman <laxman@xyz.com>
2023-07-28 04:38:03 -0700

    first commit

---
+++ src/main/java/com/project/staragile/banking/Test.java
@@ -0,0 +1,11 @@
+package com.project.staragile.banking;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("my first program");
+		// TODO Auto-generated method stub
+
+	}
+
+}
